02f6a819d6f9d312e6338a6d9c09ff01d8c6e2e1
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-02-10 20:29:42 +0530

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ForEachArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Alice");
+        names.add("Bob");
+        names.add("Charlie");
+        
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
